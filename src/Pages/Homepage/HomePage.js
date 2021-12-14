026a1fb510e35e2a1784d190d6f81fddf06f68b9
“Houssine” <person.zen15@gmail.com>
2021-12-14 23:44:37 +0100

    initial commit

---
+++ src/Pages/Homepage/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Heading } from "../../GlobalStyles";
+
+const HomePage = () => {
+  return (
+    <Heading margin="10px" inverse="red">
+      im home
+    </Heading>
+  );
+};
+
+export default HomePage;
