016631a434348cd9648ea172e19e5078e91ac5ef
“Houssine” <person.zen15@gmail.com>
2021-12-15 01:11:43 +0100

    Add data and axios

--- src/Pages/Homepage/HomePage.js
+++ src/Pages/Homepage/HomePage.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Heading } from "../../GlobalStyles";
 
+const fecthData = async () => {
+  try {
+    const data = await axios.get("data.json");
+    return data;
+  } catch (error) {
+    console.log(`error catched: ${error.message}, code: ${error.status}`);
+  }
+};
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const data = fecthData();
+    data
+      .then((data) => {
+        setProducts(data);
+        // console.log(data);
+      })
+      .catch((err) => console.log("useeffect error: " + err));
+  }, []);
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
   return (
     <Heading margin="10px" inverse="red">
-      im home
+      Im home
     </Heading>
   );
 };
